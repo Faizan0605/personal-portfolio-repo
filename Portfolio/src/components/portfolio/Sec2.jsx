9c3ff8c1c25440a4db00e478d36abb1b53f27516
faizefaizan <faizefaizan0605@gmail.com>
2025-07-30 23:24:12 +0530

    30-07-2025

--- Portfolio/src/components/portfolio/Sec2.jsx
+++ Portfolio/src/components/portfolio/Sec2.jsx
@@ -1,21 +1,37 @@
-import img1 from '../../assets1/web-developer-19.webp' 
-import img2 from '../../assets1/web-developer-15.webp' 
+import img1 from '../../assets1/web-developer-19.webp'
+import img2 from '../../assets1/web-developer-15.webp'
 function Sec2() {
-    return ( <div className="bg-black h-300 relative bottom-30 flex flex-col justify-center items-center">
-        <div className="flex justify-around items-center">
-            <div>
-                <img src={img1} alt="Web development" className="w-full h-full object-cover "></img>
-                <p className='text-zinc-200 text-xl font-bold mx-7 my-5'>DSFASFASDFAF</p>
-                <button className='border-amber-300 border-2 text-amber-300 py-2 px-5 mx-7'>VIEW PROJECT</button>    
+    return (<div className="bg-black h-auto relative  flex flex-col items-center">
+        <div className="flex justify-around items-center w-3/4 mb-14">
+            <div className='px-8'>
+                <div className='bg-zinc-900 pb-7'>
+                    <img src={img1} alt="Web development" className="w-full object-cover filter grayscale hover:grayscale-0 transition duration-200"></img>
+                    <p className='text-zinc-200 text-xl font-bold mx-7 my-5'>DSFASFASDFAF</p>
+                    <button className='border-amber-300 border-2 text-amber-300 py-2 px-5 mx-7'>VIEW PROJECT</button>
+                </div>
             </div>
-            <div> 
-                <img src={img2} alt="Web development" className="w-full h-full object-cover "></img>
-                <p className='text-zinc-200 text-xl font-bold mx-7 my-5'>DSFASFASDFAF</p>
-                <button className='border-amber-300 border-2 text-amber-300 py-2 px-5 mx-7'>VIEW PROJECT</button>
+            <div className='px-8'>
+                <div className='bg-zinc-900 pb-7'>
+                    <img src={img2} alt="Web development" className="w-full object-cover filter grayscale hover:grayscale-0 transition duration-200"></img>
+                    <p className='text-zinc-200 text-xl font-bold mx-7 my-5'>DSFASFASDFAF</p>
+                    <button className='border-amber-300 border-2 text-amber-300 py-2 px-5 mx-7'>VIEW PROJECT</button>
+                </div>
             </div>
         </div>
-       
-    </div> );
+        <div className="flex justify-around items-center w-3/4 mb-10">
+            <div className='px-8'><div className='bg-zinc-900 pb-7'>
+                <img src={img1} alt="Web development" className="w-full object-cover filter grayscale hover:grayscale-0 transition duration-200"></img>
+                <p className='text-zinc-200 text-xl font-bold mx-7 my-5'>DSFASFASDFAF</p>
+                <button className='border-amber-300 border-2 text-amber-300 py-2 px-5 mx-7'>VIEW PROJECT</button>
+            </div></div>
+            <div className='px-8'><div className='bg-zinc-900 pb-7'>
+                <img src={img2} alt="Web development" className="w-full object-cover filter grayscale hover:grayscale-0 transition duration-200"></img>
+                <p className='text-zinc-200 text-xl font-bold mx-7 my-5'>DSFASFASDFAF</p>
+                <button className='border-amber-300 border-2 text-amber-300 py-2 px-5 mx-7'>VIEW PROJECT</button>
+            </div></div>
+        </div>
+
+    </div>);
 }
 
 export default Sec2;
--- Portfolio/src/components/portfolio/Sec3.jsx
+++ Portfolio/src/components/portfolio/Sec3.jsx
@@ -1,7 +1,26 @@
 function Sec3() {
-    return ( <>
-        
-    </> );
+    return (<div className="h-auto relative bg-zinc-300 flex flex-col items-center">
+        <div className="flex justify-around items-center w-3/4 my-20">
+            <div className="px-8 w-9/10">
+                <div className="bg-zinc-50 flex flex-col justify-center items-center p-15">
+                    <p className="mb-10"> star</p>
+                    <p className="text-center">
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta assumenda eligendi quod, nesciunt, nostrum illum deserunt sunt aspernatur, laboriosam consequuntur officia quasi eum sint fugiat temporibus maiores optio laborum ea!
+                    </p>
+                </div>
+            </div>
+            <div className="px-8  w-9/10">
+                <div className="bg-zinc-50 flex flex-col justify-center items-center p-15">
+                    <p className="mb-10"> star</p>
+                    <p className="text-center">
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta assumenda eligendi quod, nesciunt, nostrum illum deserunt sunt aspernatur, laboriosam consequuntur officia quasi eum sint fugiat temporibus maiores optio laborum ea!
+                    </p>
+                </div>
+            </div>
+        </div>
+
+
+    </div>);
 }
 
 export default Sec3;
